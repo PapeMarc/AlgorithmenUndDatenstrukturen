eab4555f4ffaaed3893520be081a6deca88d9cbe
Marc Pape <marc.pape@pape-development.com>
2023-10-16 20:06:47 +0200

    Worked on Merge Sort

--- Algorithms/MergeSort.cs
+++ Algorithms/MergeSort.cs
@@ -9,20 +9,49 @@ namespace Algorithms
 {
     public class MergeSort
     {
-        private static IComparable[] input;
-        private static Vector2[] smallestUnits;
-
-        private static void LocalInit(IComparable[] toSort)
+        public static IComparable[] Sort(IComparable[] toSort)
         {
-            input = toSort;
-            if(input.Length % 2 == 0)
+            Vector2[] smallestUnits;
+
+            //------------------------------------------------------| Init
+            if (toSort.Length % 2 == 0)
             {
-                smallestUnits = new Vector2[input.Length / 2];
+                smallestUnits = new Vector2[toSort.Length / 2];
             }
             else
             {
-                smallestUnits = new Vector2[input.Length+1 / 2];
+                smallestUnits = new Vector2[toSort.Length + 1 / 2];
             }
+
+            IComparable[] output = new IComparable[toSort.Length];
+
+            //------------------------------------------------------| Merge
+
+
+            //Return Value
+            return output;
+
+        }
+
+        private static IComparable[] Merge(IComparable[] A1, IComparable[] A2)
+        {
+            int max = A1.Length >= A2.Length ? A1.Length : A2.Length;
+            IComparable[] returnValue = new IComparable[A1.Length+A2.Length];
+            int a1 = 0, a2 = 0;
+            for(int i = 0; i < max; i++)
+            {
+                if (A1[a1].CompareTo(A2[a2]) >= 0)
+                {
+                    returnValue[i] = A2[i];
+                    a2++;
+                }
+                else
+                {
+                    returnValue[i] = A1[i];
+                    a1++;
+                }
+            }
+            return returnValue;
         }
 
 
